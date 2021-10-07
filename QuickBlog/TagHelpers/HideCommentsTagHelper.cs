ef19fac09e4615757bc4f6bebbb391c912d2c027
Emil Ordanić <emordani@vsite.hr>
2021-10-07 18:46:01 +0200

    Cleanup

---
+++ QuickBlog/TagHelpers/HideCommentsTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace QuickBlog.TagHelpers
+{
+    public class HideCommentsTagHelper : TagHelper
+    {
+        public IEnumerable<object> CommentList { get; set; }
+        public int CommentCount { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "ul";
+            if (CommentList.Count() <= CommentCount)
+                output.SuppressOutput();
+        }
+    }
+}
